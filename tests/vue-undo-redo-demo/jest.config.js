b27eaab4b54d965128be41083b6f261ffc6da7db
Andrew Beng <andrewbeng89@gmail.com>
2019-03-27 13:05:14 +0100

    Use babel-jest

--- tests/vue-undo-redo-demo/jest.config.js
+++ tests/vue-undo-redo-demo/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "^.+\\.vue$": "vue-jest",
     ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$":
       "jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest"
   },
   moduleNameMapper: {
